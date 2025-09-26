982547c173907e0c7df0294288a9be3096bc8950
UgurOK <murderuo@users.noreply.github.com>
2025-09-26 16:43:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Gerekirse spesifik originler
     allow_credentials=False,
-    allow_origins=["chrome-extension://njkpjnkolglinkcgdjlhcpggbkedgbhc"],
     # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
